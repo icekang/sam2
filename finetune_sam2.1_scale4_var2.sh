300a2faf6e704b733435b6b8770d19faff5458b6
Naravich Chutisilp <naravich@mit.edu>
2025-02-01 17:57:42 -0500

    remove old evaluation system

--- finetune_sam2.1_scale4_var2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-source activate sam2
-which pythone
-
-python training/train.py \
-    -c configs/sam2.1_training/sam2.1_hiera_s_MOSE_finetune_scale4_var2.yaml \
-    --use-cluster 0 \
-    --num-gpus 2
