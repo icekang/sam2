7374cea0cf8ca954315227a2cd2010bc06465318
Naravich Chutisilp <naravich@mit.edu>
2025-03-14 15:51:57 -0400

    clean up the documentation

--- prompt_strategy_evaluation/prompter.py
+++ prompt_strategy_evaluation/prompter.py
@@ -24,7 +24,7 @@ class Prompter:
         pass
 
 
-class ConsistentPointPrompter:
+class ConsistentPointPrompter(Prompter):
     """A prompter that put the same point from the previous frame."""
 
     def __init__(
@@ -42,21 +42,34 @@ class ConsistentPointPrompter:
         ann_obj_id: int,
         mask_bool: npt.NDArray,
     ):
+        """Add a consistent point prompt.
+
+        This method save `prev_point` which is the point prompted in the previous frame (i.e. frame_idx - 1).
+        
+        Pick a new point prompt every first frame of annotation.
+        """
         mask_coords = np.argwhere(mask_bool)
-        if len(mask_coords) == 0:
+        # No positive label
+        no_positive_points_in_mask = len(mask_coords) == 0
+        if no_positive_points_in_mask:
             self.prev_center = None
             return None
+
+        is_first_frame_to_prompt = frame_idx % self.annotation_every_n == 1
+        need_new_prompt = self.prev_center is None
         if (
-            frame_idx % self.annotation_every_n == 1 or self.prev_center is None
+            is_first_frame_to_prompt or need_new_prompt
         ):  # The frame we should add a new consistent point
             center, radius = maximal_inscribed_circle(
                 mask_bool
             )  # already in x,y as they are the output from opencv
-            random_point = np.array([center])
-            self.prev_center = random_point
+            point_prompt = np.array([center])
+            self.prev_center = point_prompt
+
         assert (
             self.prev_center is not None
         ), f"prev_center should not be None at frame {frame_idx} while annotation is every {self.annotation_every_n} (modulo is {frame_idx % annotation_every_n})"
+
         labels = np.array(
             [1], np.int32
         )  # for labels, `1` means positive click and `0` means negative click
@@ -67,8 +80,7 @@ class ConsistentPointPrompter:
             points=self.prev_center,
             labels=labels,
         )
-        return {'type': 'point', 'frame': frame_idx, 'points': self.prev_center}
-        
+        return {"type": "point", "frame": frame_idx, "points": self.prev_center}
 
 
 class RandomPointPrompter(Prompter):
@@ -96,10 +108,10 @@ class RandomPointPrompter(Prompter):
             points=random_point,
             labels=labels,
         )
-        return {'type': 'point', 'frame': frame_idx, 'points': random_point}
+        return {"type": "point", "frame": frame_idx, "points": random_point}
 
 
-class MaskPrompter:
+class MaskPrompter(Prompter):
     def add_prompt(
         self,
         predictor,
