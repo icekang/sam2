8e0ae105681b937b1c341bb9d163b5cc9c82dae2
Naravich Chutisilp <naravich@mit.edu>
2025-03-22 18:00:00 -0400

    Add sam2 prediction visualization script

---
+++ prompt_strategy_evaluation/visualize_SAM2_predictions.py
@@ -0,0 +1,238 @@
+import argparse
+from functools import partial
+from pathlib import Path
+from typing import Callable
+import torch
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import numpy as np
+from PIL import Image
+from helper import get_splits, get_video_label, get_video_dir
+
+color_pallete = np.array([[0, 0, 0], [0, 255, 0], [0, 255, 0]])
+
+
+def show_points(coords, labels, ax: plt.axes, marker_size=200):
+    pos_points = coords[labels == 1]
+    neg_points = coords[labels == 0]
+    ax.scatter(
+        pos_points[:, 0],
+        pos_points[:, 1],
+        color="green",
+        marker="*",
+        s=marker_size,
+        edgecolor="white",
+        linewidth=1.25,
+    )
+    return ax.scatter(
+        neg_points[:, 0],
+        neg_points[:, 1],
+        color="red",
+        marker="*",
+        s=marker_size,
+        edgecolor="white",
+        linewidth=1.25,
+    )
+
+
+def update_animation_full(
+    ax: plt.Axes,
+    predictions: dict,
+    frame_idx2prompt: dict,
+    image_paths: list[Path],
+    groundtruth_paths: list[Path],
+    EVERY_N: int,
+    frame: int,
+) -> tuple[plt.axes, plt.axes, plt.axes]:
+    ax[2].clear()
+    ax[0].set_title(f"Image ({frame} | {image_paths[frame].name})")
+
+    if frame % EVERY_N == 0:
+        ax[2].set_title("SAM2 Prediction (mask prompted)")
+    else:
+        ax[2].set_title("SAM2 Prediction")
+
+    image = Image.open(image_paths[frame])
+    image = image.convert("RGBA")
+
+    gt = Image.open(groundtruth_paths[frame])
+    gt = gt.convert("RGBA")
+
+    prediction = Image.fromarray(
+        color_pallete[predictions[frame][1][0].astype(np.uint8)].astype(
+            np.uint8
+        )
+    )
+    prediction = prediction.convert("RGBA")
+
+    new_image = Image.blend(gt, prediction, 0.5)
+    new_image = Image.blend(image, new_image, 0.5)
+
+    ax_2_output = ax[2].imshow(prediction)
+    if frame in frame_idx2prompt:
+        prompt = frame_idx2prompt[frame]
+        prompt_type = prompt["type"]
+        if prompt_type == "mask":
+            pass
+        elif prompt_type == "point":
+            points_xy = prompt["points"]
+            N, two = points_xy.shape
+            ax_2_output = show_points(
+                coords=points_xy,
+                labels=np.array([1] * N),
+                ax=ax[2],
+                marker_size=200,
+            )
+    return ax[0].imshow(new_image), ax[1].imshow(gt), ax_2_output
+
+
+def get_update_animation(
+    ax: plt.Axes,
+    predictions: dict,
+    frame_idx2prompt: dict,
+    image_paths: list[Path],
+    groundtruth_paths: list[Path],
+    EVERY_N: int,
+) -> Callable:
+    # Partially apply the arguments except for frame, using python's functools.partial
+    return partial(
+        update_animation_full,
+        ax,
+        predictions,
+        frame_idx2prompt,
+        image_paths,
+        groundtruth_paths,
+        EVERY_N,
+    )
+
+
+def create_videos(fold: int, prediction_output: Path, EVERY_N: int):
+    splits, image_dir, groundtruth_dir = get_splits()
+
+    patients = splits[fold]["val"]
+
+    for patient_id in patients:
+        prediction_dir = prediction_output
+        prediction_path = prediction_dir / f"video_segments_{patient_id}.pt"
+        predictions = torch.load(prediction_path, weights_only=False)
+
+        prompt_path = prediction_dir / f"prompts_{patient_id}.pt"
+        prompts = torch.load(prompt_path, weights_only=False)
+        frame_idx2prompt = {prompt["frame"]: prompt for prompt in prompts}
+
+        groundtruth_path = Path(get_video_label(patient_id))
+        image_path = Path(get_video_dir(patient_id))
+
+        fig, ax = plt.subplots(1, 3, figsize=(6, 2))
+
+        image_paths = list(image_path.glob("*.jpg"))
+        image_paths.sort()
+
+        groundtruth_paths = list(groundtruth_path.glob("*.png"))
+        groundtruth_paths.sort()
+
+        ax[1].set_title("Groundtruth")
+
+        update_animation = get_update_animation(
+            ax=ax,
+            predictions=predictions,
+            frame_idx2prompt=frame_idx2prompt,
+            image_paths=image_paths,
+            groundtruth_paths=groundtruth_paths,
+        )
+
+        update_animation(0)
+        print(prediction_output, patient_id)
+        ani = animation.FuncAnimation(
+            fig=fig, func=update_animation, frames=len(image_paths)
+        )
+        ani.save(
+            prediction_output / f"{patient_id}.mp4",
+            progress_callback=lambda i, n: print("\r", i, "/", n, end=""),
+        )
+
+
+def get_show_annoation_function(
+    fold: int, prediction_output: Path, patient_idx: int, EVERY_N: int
+) -> tuple[Callable, plt.Figure, plt.Axes]:
+    """
+    Returns a function that can be used to visualize the annotation of a patient."
+    """
+    splits, image_dir, groundtruth_dir = get_splits()
+
+    patient_id = splits[fold]["val"][patient_idx]
+
+    # Load the predictions and prompts
+    prediction_dir = prediction_output
+    prediction_path = prediction_dir / f"video_segments_{patient_id}.pt"
+    predictions = torch.load(prediction_path, weights_only=False)
+
+    # Load the prompts
+    prompt_path = prediction_dir / f"prompts_{patient_id}.pt"
+    prompts = torch.load(prompt_path, weights_only=False)
+
+    # Create a dictionary that maps frame index to prompt
+    frame_idx2prompt = {prompt["frame"]: prompt for prompt in prompts}
+
+    # Load the groundtruth and image paths
+    groundtruth_path = Path(get_video_label(patient_id))
+    image_path = Path(get_video_dir(patient_id))
+
+    # Create a figure with 3 subplots
+    fig, ax = plt.subplots(1, 3, figsize=(6, 2))
+
+    # Load the image and groundtruth paths
+    image_paths = list(image_path.glob("*.jpg"))
+    image_paths.sort()
+
+    groundtruth_paths = list(groundtruth_path.glob("*.png"))
+    groundtruth_paths.sort()
+
+    ax[1].set_title("Groundtruth")
+
+    update_animation = get_update_animation(
+        ax=ax,
+        predictions=predictions,
+        frame_idx2prompt=frame_idx2prompt,
+        image_paths=image_paths,
+        groundtruth_paths=groundtruth_paths,
+        EVERY_N=EVERY_N,
+    )
+    return update_animation, fig, ax
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Evaluate video segmentation model."
+    )
+    parser.add_argument(
+        "--prompter_names",
+        nargs="+",
+        required=True,
+        help="List of prompter names to use (e.g., mask, random_point, consistent_point, k_consistent_point)",
+    )
+    parser.add_argument(
+        "--fold",
+        type=int,
+        required=True,
+        help="Fold number for cross-validation",
+    )
+    parser.add_argument(
+        "--mask_every_n",
+        type=int,
+        required=True,
+        help="Prompt mask every n frames, and prompt points in between",
+    )
+    args = parser.parse_args()
+
+    # Create the prompting strategy name
+    strategy_name = "::".join(sorted(args.prompter_names))
+    EVERY_N = args.mask_every_n
+    fold = args.fold
+
+    prediction_output_name = "fold{}_{}_annotate_every_{}".format(
+        fold, strategy_name, EVERY_N
+    )
+    prediction_output = Path(prediction_output_name)
+
+    create_videos(fold, prediction_output, EVERY_N)
